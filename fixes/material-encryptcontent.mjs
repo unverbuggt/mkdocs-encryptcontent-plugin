c34097e0daf368d2bea915c8ab44f476f30a07eb
René Rüthlein <unverbuggt@xn--rthlein-n2a.de>
2025-03-01 10:58:25 +0100

    update documentation for "material-encryptcontent: reload all subscriptions"

--- fixes/material-encryptcontent.mjs
+++
@@ -1,7 +0,0 @@
-//tested with mkdocs-material 9.5.2
-//this function is run by mkdocs-encryptcontent-plugin after successfull decryption
-function theme_run_after_decryption() {
-    document$.next(document);
-}
-
-window["theme_run_after_decryption"] = theme_run_after_decryption;
