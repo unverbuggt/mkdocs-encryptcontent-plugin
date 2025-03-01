9c4f0e91b4187fc3fba484f96099690d054da567
René Rüthlein <unverbuggt@xn--rthlein-n2a.de>
2025-03-01 10:47:14 +0100

    Add "material-encryptcontent: reload all subscriptions" fix to js template

--- encryptcontent/decrypt-contents.tpl.js
+++ encryptcontent/decrypt-contents.tpl.js
@@ -434,8 +434,10 @@ function base64url_decode(input) {
                 el.remove();
             }
         }
-
         // any post processing on the decrypted content should be done here
+    {%- if material %}
+        document$.next(document);
+    {%- else %}
         {% if arithmatex -%}
         if (typeof MathJax === 'object') { MathJax.typesetPromise();};
         {%- endif %}
@@ -447,6 +449,7 @@ function base64url_decode(input) {
             hljs.highlightElement(block);
         });
         {%- endif %}
+    {%- endif %}
         {% if reload_scripts | length > 0 -%}
         let reload_scripts = {{ reload_scripts }};
         for (let i = 0; i < reload_scripts.length; i++) { 
