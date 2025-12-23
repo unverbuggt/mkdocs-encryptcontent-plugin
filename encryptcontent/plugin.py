0287ceffd664af2a4d1730acd0e9218107ef97ad
René Rüthlein <unverbuggt@xn--rthlein-n2a.de>
2025-12-23 22:33:39 +0100

    clarify error message

--- encryptcontent/plugin.py
+++ encryptcontent/plugin.py
@@ -1078,7 +1078,7 @@ class encryptContentPlugin(BasePlugin):
                 injector = soup.new_tag("div")
                 something_search = soup.find(tag[0], {tag[1]: name})
                 if not something_search:
-                    logger.error('Could not find tag to inject!\n{name}: [{tag0}, {tag1}]'.format(tag0=tag[0], tag1=tag[1], name=name))
+                    logger.error('Could not find tag to inject! URL={url}\n{name}: [{tag0}, {tag1}]'.format(tag0=tag[0], tag1=tag[1], name=name, url=page.url))
                     os._exit(1)
                 something_search.insert_before(injector)
                 injector.append(BeautifulSoup(page.encryptcontent['decrypt_form'], 'html.parser'))
